e96bbef78cf817b6c795ee390bd6024016ff3cb0
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-15 22:06:57 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name: string = "Opi Sarker";
+  const list: number[] = [4, 5, 6, 67, 7, 78];
+
+  interface Person {
+    firstName:string,
+    lastName:string,
+    age:number
+  }
+
+  const all:Person = {
+    firstName: "Opi",
+    lastName: "Sarker",
+    age: 34,
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
