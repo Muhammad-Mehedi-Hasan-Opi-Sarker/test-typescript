3a2ab692b2bcf3ffd4fb115ac71c456392229ff6
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-15 22:10:44 +0600

    js add

---
+++ src/vannila/Vannila.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Vannila = () => {
+    const lis=(e)=>{
+        console.log(e.target.value)
+    }
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Vannila;
